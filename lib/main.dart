77ce89bdcb4a0f8ecbf5b08147065380cc0023e5
Bruno <a2020117983@estudiantes.upsa.edu.bo>
2022-03-16 17:42:46 -0400

    soy reee puto

--- lib/main.dart
+++ lib/main.dart
@@ -27,15 +27,24 @@ class inicio extends StatefulWidget {
 }
 
 class _inicioState extends State<inicio> {
+  TextEditingController idController = TextEditingController();
   TextEditingController titleController = TextEditingController();
+  TextEditingController bodyController = TextEditingController();
   Future<String> getData() async {
     var response = await http.get(
         Uri.parse("https://jsonplaceholder.typicode.com/posts"),
         headers: {"Accept": "aplication/json"});
     List map = json.decode(response.body);
     var basic = Basic.fromJson(map[1]);
+    print("id");
+    print(basic.id);
+    print("title");
     print(basic.title);
-    titleController.text = 'Titulo '+ basic.title.toString(); 
+    print("body");
+    print(basic.body);
+    idController.text = 'id ' + basic.id.toString();
+    titleController.text = 'Titulo ' + basic.title.toString();
+    bodyController.text = 'body ' + basic.body.toString();
     return 'okey';
   }
 
@@ -43,16 +52,28 @@ class _inicioState extends State<inicio> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('GutierrezApp'),
+        title: Text('LizarazuApp'),
       ),
       body: ListView(children: <Widget>[
         Text("Programa manual"),
-      TextFormField(
-        controller: titleController,
-        readOnly: true,
-        decoration: InputDecoration(hintText: "Title",
-        border:InputBorder.none),
-      ),
+        TextFormField(
+          controller: idController,
+          readOnly: true,
+          decoration:
+              InputDecoration(hintText: "ID: ", border: InputBorder.none),
+        ),
+        TextFormField(
+          controller: titleController,
+          readOnly: true,
+          decoration:
+              InputDecoration(hintText: "Title: ", border: InputBorder.none),
+        ),
+        TextFormField(
+          controller: bodyController,
+          readOnly: true,
+          decoration:
+              InputDecoration(hintText: "Body: ", border: InputBorder.none),
+        ),
         ElevatedButton(
             onPressed: getData, child: new Text('Llamando a servicio web'))
       ]),
